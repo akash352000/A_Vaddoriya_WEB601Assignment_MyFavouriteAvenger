09ecbe6fb167a74402cd1cac9e062c414f679024
akash352000 <100724977+akash352000@users.noreply.github.com>
2023-04-15 11:34:29 +0530

    initial commit
    initial commit

---
+++ src/app/helper-files/contentDb.ts
@@ -0,0 +1,13 @@
+import { Content } from "./content-interface";
+
+export class contentDb {
+    content!: Array<Content>;
+   constructor(con? : any){
+       this.content = [];
+   }
+   
+   public get contents(){
+       return this.content;
+   }
+   
+}
